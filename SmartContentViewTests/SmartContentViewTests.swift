34a997ff61ace599b682001adb560e5550365c57
Alexander Karpov <x1te.rus@gmail.com>
2016-06-17 23:54:57 +0300

    Initial Commit

---
+++ SmartContentViewTests/SmartContentViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  SmartContentViewTests.swift
+//  SmartContentViewTests
+//
+//  Created by Alexander Karpov on 17.06.16.
+//  Copyright © 2016 Stepic. All rights reserved.
+//
+
+import XCTest
+@testable import SmartContentView
+
+class SmartContentViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
