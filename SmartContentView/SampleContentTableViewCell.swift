ca13a434994aa5dbd0eb4adfc2984df2f931ed20
Alexander Karpov <x1te.rus@gmail.com>
2016-06-18 02:27:25 +0300

    added some demo files

---
+++ SmartContentView/SampleContentTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SampleContentTableViewCell.swift
+//  SmartContentView
+//
+//  Created by Alexander Karpov on 18.06.16.
+//  Copyright © 2016 Stepic. All rights reserved.
+//
+
+import UIKit
+
+class SampleContentTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var htmlContentView: HTMLContentView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- SmartContentView/ViewController.swift
+++ SmartContentView/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let strings : [String] = [
+        "I am a small simple string",
+        "Here is a string with a simple tag <b>bold</b>"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.estimatedRowHeight = 44.0
+
+        tableView.registerNib(UINib(nibName: "SampleContentTableViewCell", bundle: nil), forCellReuseIdentifier: "SampleContentTableViewCell")
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +37,22 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController : UITableViewDelegate {
+}
+
+extension ViewController : UITableViewDataSource {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return strings.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("SampleContentTableViewCell") as! SampleContentTableViewCell
+        
+        cell.htmlContentView.loadHTMLText(strings[indexPath.row])
+        return cell
+    }
+}
